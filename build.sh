d0c9cf106c603575159db11826865966bd097b9f
Daniel Brice <dbrice@cj.com>
2017-06-08 15:20:43 -0700

    pandoc already does all that stuff

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-mkdir -p texbuild
-mkdir -p target
-cp -r img texbuild/img
-
-pandoc --from=markdown --to=beamer --standalone \
-  < presentation.md > texbuild/presentation.tex
-
-cd texbuild
-pdflatex -interaction=batchmode -halt-on-error presentation.tex
-cd ..
-mv texbuild/presentation.pdf presentation.pdf
+pandoc --to=beamer --output=presentation.pdf presentation.md
