372f729f55a48a2c3d6cb2b68114f8832803bcc2
Daniel Brice <dbrice@cj.com>
2017-06-08 18:02:17 -0700

    fix pandoc invocation for summary

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pandoc --to=html5 --output=summary.html summary.md
+pandoc --to=html5 --standalone --mathjax --output=summary.html summary.md
 pandoc --to=beamer --output=presentation.pdf presentation.md
 cd demo
 ghc Sampler.hs
